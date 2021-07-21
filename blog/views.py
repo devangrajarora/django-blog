30ab92c6d8b9213863638e717fb0cb86536929bf
Devang Arora <devangraj.arora@gmail.com>
2021-07-21 15:31:37 +0530

    setup

---
+++ blog/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+posts = [
+    {
+        'title': 'Leetcode Solutions',
+        'author': 'Devang',
+        'content': 'DFS',
+        'date': 'July 21, 2021'
+    },
+    {
+        'title': 'Football',
+        'author': 'Devang Arora',
+        'content': 'Copa America',
+        'date': 'July 22, 2021'
+    }
+]
+
+
+def hello(request):
+    # print("Request: ", request.method)
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+
+def about(request):
+    return render(request, 'blog/about.html')
